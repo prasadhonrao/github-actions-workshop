36c0466994ea9662d1b34dbcce59de2a69809426
prasadhonrao <honrao.prasad@gmail.com>
2025-06-09 10:03:36 +0100

    feat: Update server to use ES module syntax and correct greeting message

--- src/javascript/express-hello-world/server.js
+++ src/javascript/express-hello-world/server.js
@@ -1,4 +1,7 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello with caching!'));
+
+app.get('/', (req, res) => res.send('Hello from express server!'));
+
 app.listen(3000, () => console.log('Running on port 3000'));
